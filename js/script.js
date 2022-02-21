03c6295c26084403398067360b232aac00ad6909
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-02-21 15:11:05 +0100

    progressi

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let chilometri = prompt("chilometri da percorrere:", "scrivi quanti chilometri dovrai percorrere");
+let eta = prompt("Età", "scrivi quanti anni hai");
+let prezzoTotale = (chilometri * 0.21)
+
